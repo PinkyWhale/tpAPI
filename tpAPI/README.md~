a375ad27e6946d70c00901190cb9c96189932f2d
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-21 10:29:49 -0300

    separe los js

---
+++ tpAPI/README.md~
@@ -0,0 +1,10 @@
+TP API
+======
+
+Para levantar el server hay que ir a la carpeta js, abrir la termianl y escribir lo siguiente
+
+	- pinky@pingui ~/Ada/tpAPI (git)-[master] % cd js
+	- pinky@pingui ~/Ada/tpAPI/js (git)-[master] % node server.js
+Escuchando en el puerto 5050
+
+
--- tpAPI/js/main.js
+++ tpAPI/js/main.js
@@ -1,27 +1,2 @@
-//paquetes necesarios para el proyecto
-var http = require('http');
-var fs = require('fs');
-//var miControlador = require('./controladores/funciones');
-
-//seteamos el puerto en el cual va a escuchar los pedidos la aplicación
-var puerto = '5050';
-
-http.createServer(function(req,res){
-//res.writeHead(200,{'Content-type':'text/html'});
-
-if(req.url == "/index"){
-
-	fs.readFile('html/index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-
-}else{
-	res.write("Bienvenidos");
-	res.end();
-}
-
-}).listen(puerto, function () {
-  console.log( "Escuchando en el puerto " + puerto );
-});
+	//- ID de Cliente
+	//- ojq7x24ftqbzx1uevy6o5had2c5mbc
