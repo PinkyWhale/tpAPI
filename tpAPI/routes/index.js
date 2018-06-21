0babad339caf8f4f77986482c7baf63d264fa5fe
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-21 10:06:15 -0300

    saque express

--- tpAPI/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* ID de cliente ojq7x24ftqbzx1uevy6o5had2c5mbc */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TPcharlaAPI' });
-});
-
-module.exports = router;
