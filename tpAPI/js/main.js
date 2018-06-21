935739cc96d5d19c3c042dedaa20d38eec863d5e
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-21 12:46:24 -0300

    segundo GET

--- tpAPI/js/main.js
+++ tpAPI/js/main.js
@@ -17,4 +17,27 @@ $(document).ready(function(){
 			};	
 		}
 	});
+
+	$.ajax({
+		type: "GET",
+		url:'https://api.twitch.tv/helix/streams?first=20/follows/channels/',
+		headers:{
+			'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
+		success:function(data1){
+			debugger;
+			for (var i = 0; i < data2.follows.length; i++) {
+				console.log(data2.follows[0]);
+				  //gets displayName
+				  var displayName = data2.follows[i].channel.display_name;
+				  var logo = data2.follows[i].channel.logo;
+				var status= data2.follows[i].channel.status;
+				$("#favUs").prepend("<div class ='row'>" + "<div class='logoF'>" +
+              "<a href='https://www.twitch.tv/"+ displayName+"'><img src='" + logo + "'></a>"
+              +
+              "</div>" + "<div class='usuarioF'>" + displayName + "</div>" + "<div class='statusF'>" + status + "</div></div>");
+			}
+		}
+	});
+
+	
 });
