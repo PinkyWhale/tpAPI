6c2cb5d3598491f9e29cabe94b68026fe93ed249
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-21 09:50:34 -0300

    ID key

--- tpAPI/routes/index.js
+++ tpAPI/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* ID de cliente ojq7x24ftqbzx1uevy6o5had2c5mbc */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TPcharlaAPI' });
 });
 
 module.exports = router;
