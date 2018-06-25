8b9c84f119f9dec10faa1483ad5c4d93033de895
elifer aponte <eliferaponte_11@hotmail.com>
2018-06-24 13:14:55 -1200

    borrando debuggers

--- public/js/main.js
+++ public/js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 		headers:{
 			'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
 		success:function(data1){
-			debugger;
+			
 			if(data1.stream === null){
 				$("#userStatus").html("STREAMER off");
 			}else{
@@ -41,7 +41,7 @@ $(document).ready(function(){
 		url:'https://api.twitch.tv/kraken/users/Pink_Whale/follows/channels/',
 		headers:{'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
 		success:function(data2){
-			debugger;
+			
 			for (var i = 0; i < data2.follows.length; i++) {
 				console.log(data2.follows[0]);
 					var displayName = data2.follows[i].channel.display_name;
@@ -60,7 +60,7 @@ $(document).ready(function(){
 		url: "https://api.twitch.tv/kraken/users/Pink_Whale?client_id=ojq7x24ftqbzx1uevy6o5had2c5mbc",
 		headers:{'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
 		success:function(data3){
-			debugger;
+			
 			console.log(data3.display_name);
 			$("#userName").html(data3.display_name);
 			
@@ -72,7 +72,7 @@ $(document).ready(function(){
 		url: "https://api.twitch.tv/kraken/users/Pink_Whale?client_id=ojq7x24ftqbzx1uevy6o5had2c5mbc",
 		headers:{'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
 		success:function(data4){
-			debugger;
+			
 			console.log(data4.created_at);
 			$("#createDate").html(data4.created_at);
 
@@ -84,7 +84,7 @@ $(document).ready(function(){
 		url: "https://api.twitch.tv/kraken/users/Pink_Whale?client_id=ojq7x24ftqbzx1uevy6o5had2c5mbc",
 		headers:{'Client-ID': 'ojq7x24ftqbzx1uevy6o5had2c5mbc'},
 		success:function(data5){
-			debugger;
+			
 			console.log(data5.logo); // en console de html aparece la url pero no puedo hacerlo aparecer
 			$(".part1").prepend("<img src='" + data5.logo + "'>");
 
